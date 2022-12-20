daae4dcf5e4226f850ee176c58c212d06461113f
darshanmarathe <darshanmarathe@gmail.com>
2022-12-20 12:04:45 +0530

    Process PORT

--- Website/server.js
+++ Website/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
-const port = 3000;
+const port = process.env.PORT ||  3000;
 app.use(express.static('./'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
